c08cf5182dff09962557fe8ef42273d43ebed737
Rhoward1 <ryanmhoward@gmail.com>
2022-05-08 17:22:26 -0500

    working on routes and added more file structure

--- server.js
+++ server.js
@@ -2,20 +2,38 @@ const express = require('express');
 const path = require('path');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-const sequelize = require('./config/connections');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({helpers});
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const hbs = exphbs.create({});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+  secret: 'itsasecret',
+  cookie: {
+        
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
+
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
