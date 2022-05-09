28de5042b343b43bc03bb16f6090ce367d029550
Rhoward1 <ryanmhoward@gmail.com>
2022-05-08 23:46:47 -0500

    login working and main handlebars set up.

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const path = require('path');
 const routes = require('./controllers');
-const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
+const path = require('path');
 const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({helpers});
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
 
-const PORT = process.env.PORT || 3001;
+const session = require('express-session');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -28,16 +29,17 @@ const sess = {
 };
 
 app.use(session(sess));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(routes);
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
